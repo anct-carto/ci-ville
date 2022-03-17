39838782b2cc6b81fc37ddd6898e0bca1ffaa9a0
HassChg <52163102+HassChg@users.noreply.github.com>
2022-03-17 11:31:01 +0100

    publicPath nul

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production'
-      ? '/' + 'vie-associative' + '/'
-      : '/'
-  }
+    // publicPath: process.env.NODE_ENV === 'production'
+    //   ? '/' + 'vie-associative' + '/'
+    //   : '/'
+    publicPath:''
+}
