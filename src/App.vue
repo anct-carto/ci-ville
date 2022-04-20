6dc9ef72306fc44f8ec3c22f9d934739a9ba8d46
HassChg <52163102+HassChg@users.noreply.github.com>
2022-04-20 16:07:56 +0200

    debut redaction page a propos

--- src/App.vue
+++ src/App.vue
@@ -116,6 +116,7 @@ html, body {
   text-decoration: none;
   display: block;
   text-align: center;
+  transition: color .15s ease-in-out,background-color .15s ease-in-out,border-color .15s ease-in-out,box-shadow .15s ease-in-out !important;
 }
 
 .dropdown-menu {
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,10 +1,39 @@
 <template>
-  <h1>Sources, données, nomenclature, contacts ... etc.</h1>
+  <div>
+    <h3>Source des données et licence</h3>
+    <p>Dauphin, GISPro, lien vers data-gouv ... etc.</p>
+    <h3>Réalisation</h3>
+    <p>Service cartographie de l'ANCT avec l'appui de la mission Vie associative + code source</p>
+    <h3>Contact</h3>
+    <p>adresses email</p>
+    <h3>Liens utiles</h3>
+    <ul>
+      <li>Lien 1</li>
+      <li>Lien 2</li>
+    </ul>
+  </div>
+  <div class="footer">
+        <img :src="logo_anct" alt="" id="logo-anct"><br><br>
+  </div>
 </template>
 
 <script>
+import logo_anct from "@/assets/logo_anct.png"
 
 export default {
   name: 'About',
+  data() {
+    return {
+      logo_anct:logo_anct,
+    }
+  }
 }
 </script>
+
+<style scoped>
+  #logo-anct {
+    bottom:0px;
+    width:400px;
+  }
+
+</style>
