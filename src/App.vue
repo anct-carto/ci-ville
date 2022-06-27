62e8bc80ccac932cec3232a58410906bff0a1fda
HassChg <hassen.chougar@anct.gouv.fr>
2022-06-27 15:04:03 +0200

    ajout spinner chargement

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,14 @@
           <router-link to="/a-propos">À propos</router-link> 
         </div>
       <div class="row app-view">
+        <div id="loading" v-if="!this.$store.state.filteredData">
+            <div class="row">
+                <div class="spinner-border" role="status"></div>
+            </div>
+            <div class="row">
+                <p>Chargement en cours ...</p>
+            </div>
+        </div>
         <router-view/>
       </div>
     </div>
@@ -126,7 +134,7 @@ html, body {
 
 #nav a.router-link-active {
   font-family: 'Marianne-Bold';
-  border-bottom: solid 3px var(--rose-gerr);
+  border-bottom: solid 2px var(--rose-gerr);
   border-bottom-width: 50% !important;
 }
 
@@ -138,6 +146,23 @@ html, body {
 select,option {
   cursor:pointer
 }
+
+#loading {
+  position: absolute;
+  justify-content: center;
+  align-items: center;
+  display: flex;
+  left:0;
+  top:0;
+  width: 100%;
+  height: 100vh;
+  background: rgba(0,0,0,.7);
+  z-index: 20000;
+  color:white;
+  flex-direction: column;
+  overflow-x: hidden;
+}
+
 
 @import'~bootstrap/dist/css/bootstrap.css'
 
