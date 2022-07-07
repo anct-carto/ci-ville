25c3921fd7e8d565f19134ea9cc43a871f457b9d
HassChg <52163102+HassChg@users.noreply.github.com>
2022-07-07 16:51:55 +0200

    build -- env mode to production

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,18 @@
+const webpack = require('webpack')
+// const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
 module.exports = {
     // publicPath: process.env.NODE_ENV === 'production'
     //   ? '/' + 'vie-associative' + '/'
     //   : '/'
-    publicPath:''
+    publicPath:'',
+    configureWebpack: {
+        plugins: [
+            // ...
+            new webpack.DefinePlugin({
+              'process.env.NODE_ENV': JSON.stringify('production')
+            }),
+            // new BundleAnalyzerPlugin()
+        ],
+    }
 }
