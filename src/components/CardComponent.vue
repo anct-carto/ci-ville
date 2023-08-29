37b501a554bc4f69258afea1c9ddc9afc7c50750
HassChg <52163102+HassChg@users.noreply.github.com>
2023-08-29 14:58:22 +0200

    suppression éléments commentés

--- src/components/CardComponent.vue
+++ src/components/CardComponent.vue
@@ -85,11 +85,9 @@ export default {
 .card-tooltip {
     margin-left: auto;
     position:fixed;
-    /* background: rgb(252, 252, 252); */
     background: #5473b6;
     color:white;
     box-shadow: 0 2px 2px rgba(0,0,0,.09), 0 0px 2px rgba(0,0,0,.1);
-    /* z-index: 1000; */
     max-width: 350px;
     z-index: 500;
     padding: 5px 10px 5px 10px;
