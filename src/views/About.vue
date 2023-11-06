5620a134cfc8acc6e713bac6726d6a8d75a6bcc7
HassChg <52163102+HassChg@users.noreply.github.com>
2023-11-06 18:11:13 +0100

    MAJ texte à propos

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="row">
-    <div class="col-8">
+    <div class="col-9">
       <h3>Qu'est-ce que Ci.Ville ?</h3>
       <p>
         L’État attribue annuellement des subventions à des porteurs de projets œuvrant en faveur des habitants des quartiers prioritaires de la politique de la ville. Les cartes interactives permettent de visualiser, par année, la répartition géographique et thématique de ces subventions.
@@ -24,19 +24,21 @@
       </p>
       <h3>Quelles sont les données représentées ?</h3>
       <p>
-        Les données sont issues des systèmes d'information de l'État.
-        Elles représentent le montant financé sous forme de subvention par le programme 147 - politique de la ville,
-        les actions déployées ainsi que les tiers concernés. Les postes adultes relais et Fonjep ne sont pas représentés.
+        Les données sont issues des systèmes d'information de l'État, de l'ASP et du Fonjep. 
+        Elles représentent le montant financé sous forme de subvention par le programme 147 - politique de la ville, les actions déployées ainsi que les tiers concernés. 
+      </p>
+      <p>Les aides aux postes Fonjep sont matérialisées par les effectifs présents au 31/12/N  enregistrés à date avec les coûts (théoriques) associés.
+        <br>Les aides aux postes adultes-relais  correspondent aux conventions signées et actives au 31/12/N  enregistrées  à l'ASP à date avec les coûts (théoriques) associés. 
+        Ces postes ne sont pas toujours pourvus (vacances liées au délai de recrutement par exemple). De plus, il peut s'agir de poste à temps partiel et non à temps plein.
       </p>
       <p>
-        Les cercles figurant sur les cartes sont proportionnels au montant octroyé.
+        Les cercles figurant sur les cartes sont proportionnels au montant de subventions octroyé.
       </p>
       <p>
         La liste des actions financées permet d’accéder au libellé des actions ainsi qu’au nom des porteurs de projet.
       </p>
       <p>
         Le nombre d’habitants en QPV est à comprendre comme étant la population municipale vivant en QPV au dernier recensement de la population de l'Insee (RP 2018, recensement exhaustif de 2017 pour Mayotte). 
-        <br>
         Sur la visualisation, ce chiffre ne varie qu'en fonction du territoire sélectionné et n'est en aucune  manière dépendant du filtre "pilier".
       </p>
       <!-- titres à changer "programme 147" => "crédits de la politique de la ville" -->
